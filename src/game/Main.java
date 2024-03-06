0b1a4a0a019f9335b5d16ded6c6bd67ee2330197
Jesús <jesus.garciap@iesnervion.es>
2024-03-06 20:32:36 +0100

    Falta optimizar y comentar

--- src/game/Main.java
+++ src/game/Main.java
@@ -12,6 +12,7 @@ public class Main {
 
 		int[][] tableroDejuego = Memory.getTableroDeJuego();
 
+
 		// Primera posición seleccionada por el jugador
 		int primeraPosicionSeleccionada;
 		// Segunda posición seleccionada por el jugador
@@ -31,12 +32,15 @@ public class Main {
 			primeraPosicionSeleccionada = -1;
 			segundaPosicionSeleccionada = -1;
 			
+			System.out.println("Pulsa intro para continuar");
+			sc.nextLine();
+			
 			Memory.muestraTableroDeJuego();
 
 			while (primeraPosicionSeleccionada < 0
-					|| primeraPosicionSeleccionada / 10 >= Memory.getTableroDeJuego().length
-					|| primeraPosicionSeleccionada % 10 >= Memory.getTableroDeJuego()[0].length
-					|| Memory.getTableroDeJuego()[primeraPosicionSeleccionada / 10][primeraPosicionSeleccionada
+					|| primeraPosicionSeleccionada / 10 >= tableroDejuego.length
+					|| primeraPosicionSeleccionada % 10 >= tableroDejuego[0].length
+					|| tableroDejuego[primeraPosicionSeleccionada / 10][primeraPosicionSeleccionada
 							% 10] != 0) {
 				// Le pedimos al usuario la primera posición
 				System.out.println("Elija una posición");
@@ -50,13 +54,13 @@ public class Main {
 			
 			System.out.println("FILA " + primeraPosicionSeleccionada / 10 + ", COLUMNA "
 					+ primeraPosicionSeleccionada % 10 + " = "
-					+ Memory.getTableroDeJuego()[primeraPosicionSeleccionada / 10][primeraPosicionSeleccionada % 10]);
+					+ tableroDejuego[primeraPosicionSeleccionada / 10][primeraPosicionSeleccionada % 10]);
 
 		
 			while (segundaPosicionSeleccionada < 0
-					|| segundaPosicionSeleccionada / 10 >= Memory.getTableroDeJuego().length
-					|| segundaPosicionSeleccionada % 10 >= Memory.getTableroDeJuego()[0].length
-					|| Memory.getTableroDeJuego()[segundaPosicionSeleccionada / 10][segundaPosicionSeleccionada
+					|| segundaPosicionSeleccionada / 10 >= tableroDejuego.length
+					|| segundaPosicionSeleccionada % 10 >= tableroDejuego[0].length
+					|| tableroDejuego[segundaPosicionSeleccionada / 10][segundaPosicionSeleccionada
 							% 10] != 0) {
 				// Le pedimos al usuario la segunda posición
 				System.out.println("Elija otra posición");
@@ -70,16 +74,16 @@ public class Main {
 			
 			System.out.println("FILA " + segundaPosicionSeleccionada / 10 + ", COLUMNA "
 					+ segundaPosicionSeleccionada % 10 + " = "
-					+ Memory.getTableroDeJuego()[segundaPosicionSeleccionada / 10][segundaPosicionSeleccionada % 10]);
+					+ tableroDejuego[segundaPosicionSeleccionada / 10][segundaPosicionSeleccionada % 10]);
 
 			parejaIgual = Memory.comprobarPareja(primeraPosicionSeleccionada, segundaPosicionSeleccionada);
 
 			System.out.println(parejaIgual ? "Genial, has encontrado una pareja" : "Fallo, son distintos");
 
 			sc.nextLine();
-			System.out.println("Pulsa intro para continuar");
-			sc.nextLine();
+
 		}
+		System.out.println("ENHORABUENA, has completado el juego");
 
 		sc.close();
 	}
