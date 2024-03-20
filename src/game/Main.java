374a8ff4b78f2312e4f3dfb7ea2d8d1eb8555b43
Jesús <jesus.garciap@iesnervion.es>
2024-03-20 06:24:47 +0100

    Con todo comentado

--- src/game/Main.java
+++ src/game/Main.java
@@ -12,7 +12,14 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		// Tamaño del tablero
+		int tamTablero;
+		// Tablero de juego que se muestra en pantalla
 		int[][] tableroDejuego;
+		// Dificultad seleccionada por el usuario
+		String dificultad = "";
+		// Variable que determina si una pareja de números son iguales
+		boolean parejaIgual;
 
 		// Primera fila seleccionada por el jugador
 		int primeraFila;
@@ -23,123 +30,164 @@ public class Main {
 		// Segunda columna seleccionada por el jugador
 		int segundaColumna;
 
-		String dificultad = "";
-		boolean parejaIgual;
-
+		// Mientras la dificultad no sea ni easy, ni medium ni hard
 		while (!dificultad.toLowerCase().equals("easy") && !dificultad.toLowerCase().equals("medium")
 				&& !dificultad.toLowerCase().equals("hard")) {
+			// Le pedimos al usuario una dificultad
 			System.out.println("Elija dificultad: easy, medium or hard");
+			// Y la asignamos
 			dificultad = sc.nextLine();
 		}
-
-		Memory.asignarDificultad(dificultad.toLowerCase());
 		
+		// Una vez seleccionada la dificultad, inicializamos las propiedades
+		Memory.inicializar(dificultad.toLowerCase());
+	
+		// Asignamos los valores elegidos a las variables tamTablero y tableroDeJuego
+		tamTablero = Memory.getTamTablero();
 		tableroDejuego = Memory.getTableroDeJuego();
 
-		// Llamamos a la función generaSolucion() para asignar los valores aleatorios de
-		// la tabla solucion
+		// Asignamos los valores de la propiedad del tablero solución
 		Memory.generaSolucion();
 
-		while (!Memory.finDeJuego()) { // empieza
+		// Mientras el juego no haya terminado
+		while (!Memory.finDeJuego()) { 
 
 			System.out.println("Pulsa intro para continuar");
 			sc.nextLine();
 
+			// Mostramos el tablero de juego
 			Memory.muestraTableroDeJuego();
 
 			do {
+				// Inicializamos la fila y la columna del primer número
 				primeraFila = -1;
 				primeraColumna = -1;
-				while (primeraFila < 0 || primeraFila >= Memory.getTamTablero()) {
+				
+				// Mientras la primera fila no esté entre los números elegibles
+				while (primeraFila < 0 || primeraFila >= tamTablero) {
 					try {
-						// Le pedimos al usuario la primera posición
+						// Le pedimos al usuario la primera fila
 						System.out.println("Elija una fila");
 						// Y la asignamos
 						primeraFila = sc.nextInt();
+						// Si no se escribe un número entero
 					} catch (InputMismatchException e) {
+						// Muestra este mensaje
 						System.out.println("Sólo se admiten números enteros");
 					} finally {
+						// Siempre limpiamos el buffer
 						sc.nextLine();
 					}
 
 				}
 
-				while (primeraColumna < 0 || primeraColumna >= Memory.getTamTablero()) {
+				// Mientras la primera columna no esté entre los números elegibles
+				while (primeraColumna < 0 || primeraColumna >= tamTablero) {
 					try {
-						// Le pedimos al usuario la primera posición
+						// Le pedimos al usuario la primera columna
 						System.out.println("Elija una columna");
 						// Y la asignamos
 						primeraColumna = sc.nextInt();
+						// Si no se escribe un número entero
 					} catch (InputMismatchException e) {
+						// Muestra este mensaje
 						System.out.println("Solo se admiten números enteros");
 					} finally {
+						// Siempre limpiamos el buffer
 						sc.nextLine();
 					}
 
 				}
 
-				if (Memory.getTableroDeJuego()[primeraFila][primeraColumna] != 0) {
+				// Si la posición elegida corresponde a un número distinto de 0
+				if (tableroDejuego[primeraFila][primeraColumna] != 0) {
+					// Es porque dicha posición ya está descubierta, lo indicará
 					System.out.println("Esa posición ya está descubierta");
 				}
 
-			} while (Memory.getTableroDeJuego()[primeraFila][primeraColumna] != 0);
+				// Mientras la posición indicada no sea distinto de 0, pedirá de nuevo la fila y la columna
+			} while (tableroDejuego[primeraFila][primeraColumna] != 0);
 
+			// Una vez elegidos los valores correctos, descubrimos el número oculto
 			Memory.descubrirCasillaTablero(primeraFila, primeraColumna);
 
+			// Mostramos de nuevo el tablero de juego con el número que se acaba de descubrir
 			Memory.muestraTableroDeJuego();
 
+			// Mostramos un mensaje indicando la opción elegida
 			System.out.println("FILA " + primeraFila + ", COLUMNA " + primeraColumna + " = "
 					+ tableroDejuego[primeraFila][primeraColumna]);
 
 			do {
+				// Inicializamos la fila y la columna del segundo número
 				segundaFila = -1;
 				segundaColumna = -1;
-				while (segundaFila < 0 || segundaFila >= Memory.getTamTablero()) {
+				
+				// Mientras la segunda fila no esté entre los números elegibles
+				while (segundaFila < 0 || segundaFila >= tamTablero) {
 					try {
-						// Le pedimos al usuario la segunda posición
+						// Le pedimos al usuario la segunda fila
 						System.out.println("Elija una fila");
 						// Y la asignamos
 						segundaFila = sc.nextInt();
+						// Si no se escribe un número entero
 					} catch (InputMismatchException e) {
+						// Muestra este mensaje
 						System.out.println("Sólo se admiten números enteros");
 					} finally {
+						// Siempre limpiamos el buffer
 						sc.nextLine();
 					}
 				}
 
-				while (segundaColumna < 0 || segundaColumna >= Memory.getTamTablero()) {
+				// Mientras la segunda columna no esté entre los números elegibles
+				while (segundaColumna < 0 || segundaColumna >= tamTablero) {
 					try {
-						// Le pedimos al usuario la primera posición
+						// Le pedimos al usuario la primera columna
 						System.out.println("Elija una columna");
 						// Y la asignamos
 						segundaColumna = sc.nextInt();
+						// Si no se escribe un número entero
 					} catch (InputMismatchException e) {
+						// Muestra este mensaje
 						System.out.println("Solo se admiten números enteros");
 					} finally {
+						// Siempre limpiamos el buffer
 						sc.nextLine();
 					}
 
 				}
-				if (Memory.getTableroDeJuego()[segundaFila][segundaColumna] != 0) {
+				
+				// Si la posición elegida corresponde a un número distinto de 0
+				if (tableroDejuego[segundaFila][segundaColumna] != 0) {
+					// Es porque dicha posición ya está descubierta, lo indicará
 					System.out.println("Esa posición ya está descubierta");
 				}
-			} while (Memory.getTableroDeJuego()[segundaFila][segundaColumna] != 0);
+				
+				// Mientras la posición indicada no sea distinto de 0, pedirá de nuevo la fila y la columna
+			} while (tableroDejuego[segundaFila][segundaColumna] != 0);
 
+			// Una vez elegidos los valores correctos, descubrimos el número oculto
 			Memory.descubrirCasillaTablero(segundaFila, segundaColumna);
 
+			// Mostramos de nuevo el tablero de juego con el número que se acaba de descubrir
 			Memory.muestraTableroDeJuego();
 
+			// Mostramos un mensaje indicando la opción elegida
 			System.out.println("FILA " + segundaFila + ", COLUMNA " + segundaColumna + " = "
 					+ tableroDejuego[segundaFila][segundaColumna]);
 
+			// Comprobamos la pareja y asignamos true o false a parejaIgual
 			parejaIgual = Memory.comprobarPareja(primeraFila, primeraColumna, segundaFila, segundaColumna);
-
+			// Según su valor, indicará si son iguales o no
 			System.out.println(parejaIgual ? "Genial, has encontrado una pareja" : "Fallo, son distintos");
 
-		} // termina
+		} 
 
+		// Mensaje de finalización del juego
 		System.out.println("ENHORABUENA, has completado el juego");
 
+		// Cerramos el Scanner
 		sc.close();
 	}
 
--- src/game/Memory.java
+++ src/game/Memory.java
@@ -7,8 +7,6 @@ import java.util.Random;
  */
 public class Memory {
 
-	static Random rand = new Random();
-
 	/**
 	 * Tamannio del tablero
 	 */
@@ -24,6 +22,11 @@ public class Memory {
 	 */
 	private static int[][] tableroDeJuego;
 
+	/**
+	 * Funcion que devuelve el valor del atributo tamannio del tablero
+	 * 
+	 * @return Valor del tamannio del tablero
+	 */
 	public static int getTamTablero() {
 		return tamTablero;
 	}
@@ -41,20 +44,34 @@ public class Memory {
 		return tableroDeJuego;
 	}
 
-	public static void asignarDificultad(String dificultad) {
+	/**
+	 * Funcion que inicializa todos los atributos, asignando los valores
+	 * correspondientes segun la dificultad elegida
+	 * 
+	 * @param dificultad Dificultad elegida por el usuario
+	 */
+	public static void inicializar(String dificultad) {
+		// Según el valor de la dificultad
 		switch (dificultad) {
+		// Si es "easy"
 		case "easy":
+			// El tamaño del tablero será 4
 			tamTablero = 4;
 			break;
+		// Si es "medium"
 		case "medium":
+			// El tamaño del tablero será 6
 			tamTablero = 6;
 			break;
+		// Si es "hard"
 		case "hard":
+			// El tamaño del tablero será 8
 			tamTablero = 8;
 			break;
 
 		}
 
+		// Inicializo las dos tablas con el tamaño de tablero asignado
 		solucion = new int[tamTablero][tamTablero];
 		tableroDeJuego = new int[tamTablero][tamTablero];
 
@@ -65,6 +82,10 @@ public class Memory {
 	 * en posiciones aleatorias, pero no sera visible
 	 */
 	public static void generaSolucion() {
+
+		// Creamos un objeto de tipo Random
+		Random rand = new Random();
+
 		// Numero que ocupará un lugar en la tabla, inicializado en 1
 		int numParaRellenar = 1;
 
@@ -125,12 +146,15 @@ public class Memory {
 	public static void muestraTableroDeJuego() {
 		System.out.println("\t\tCOLUMN");
 		System.out.print("\t\t");
+
+		// Números que representan a las posiciones de las columnas
 		for (int j = 0; j < tableroDeJuego[0].length; j++) {
 			System.out.print(j + "\t");
 		}
 
 		System.out.println();
 
+		// Línea de guiones según el tamaño del tablero
 		switch (tamTablero) {
 		case 4:
 			System.out.println("\t\t-------------------------");
@@ -143,13 +167,20 @@ public class Memory {
 			break;
 		}
 
+		// Mostramos el tablero de juego
+		// Bucle for que recorre cada fila
 		for (int i = 0; i < tableroDeJuego.length; i++) {
+			// Si estamos en la primera fila, muestra la palabra ROW
 			System.out.print((i == 0 ? "ROW" : "") + "\t");
+			// Bucle for que recorre cada columna
 			for (int j = 0; j < tableroDeJuego[i].length; j++) {
+				// Si estamos en la primera columna
 				if (j == 0) {
+					// Muestra antes el número de fila en que nos encontramos
 					System.out.print(i + "|\t");
 				}
-				// Lo muestra sólo si es distinto de 0
+				// Muestra el número sólo si es distinto de 0, en caso contrario muestra un
+				// cuadrado
 				System.out.print((tableroDeJuego[i][j] != 0 ? tableroDeJuego[i][j] : "\u25A0") + "\t");
 			}
 			System.out.println();
@@ -183,50 +214,61 @@ public class Memory {
 
 	}
 
+	/**
+	 * Funcion que indica si la pareja de numeros que hemos descubierto son iguales.
+	 * Si no lo son, los vuelve a "ocultar" asignandoles 0
+	 * 
+	 * @param primeraFila    Fila del primer numero
+	 * @param primeraColumna Columna del primer numero
+	 * @param segundaFila    Fila del segundo numero
+	 * @param segundaColumna Columna del segundo numero
+	 * @return True o false segun si la pareja de numeros son iguales o no
+	 */
 	public static boolean comprobarPareja(int primeraFila, int primeraColumna, int segundaFila, int segundaColumna) {
+		// Variable que indica si los números son iguales
 		boolean sonIguales = true;
 
+		// Si ambos números descubiertos son diferentes
 		if (tableroDeJuego[primeraFila][primeraColumna] != tableroDeJuego[segundaFila][segundaColumna]) {
+			// Asignamos 0 a ambos números
 			tableroDeJuego[primeraFila][primeraColumna] = 0;
 			tableroDeJuego[segundaFila][segundaColumna] = 0;
+			// Y el boolean como false
 			sonIguales = false;
 		}
 
+		// Devolverá el valor del boolean
 		return sonIguales;
 
 	}
 
 	/**
-	 * Funcion que indica si hemos terminado el juego
+	 * Funcion que indica si hemos terminado el juego, lo determina buscando un 0 en
+	 * el tablero de juego
 	 * 
 	 * @return True o false segun si el juego se ha terminado o no
 	 */
 	public static boolean finDeJuego() {
-		// Variable que determina si el juego ha terminado, por ahora falso
-		boolean juegoTerminado = false;
+		// Variable que determina si el juego ha terminado, por ahora verdadero
+		boolean juegoTerminado = true;
 
 		// Número a encontrar en el tablero: el 0
 		int numEncontrar = 0;
 
-		// Posiciones de la tabla tableroDeJuego (inicializadas en -1)
-		int posX = -1;
-		int posY = -1;
-
 		// Contador de las filas
 		int i = 0;
-		// Mientras el contador de filas sea menor al número de filas del tableroDeJuego
-		// y posX sea -1
-		while (i < tableroDeJuego.length && posX == -1) {
+		// Mientras el contador de filas sea menor al número de filas del
+		// tableroDeJuego y el boolean sea true
+		while (i < tableroDeJuego.length && juegoTerminado) {
 			// Contador de las columnas
 			int j = 0;
 			// Mientras el contador de columnas sea menor al número de columnas del
-			// tableroDeJuego y posY sea -1
-			while (j < tableroDeJuego[i].length && posY == -1) {
+			// tableroDeJuego y el boolean sea true
+			while (j < tableroDeJuego[i].length && juegoTerminado) {
 				// Si el número que buscamos está presente en el tableroDeJuego
 				if (tableroDeJuego[i][j] == numEncontrar) {
-					// Le asignamos las posiciones en donde ha sido encontrado
-					posX = i;
-					posY = j;
+					// Asignamos el boolean como false
+					juegoTerminado = false;
 				}
 				// Incrementamos el contador de las columnas
 				j++;
@@ -234,11 +276,6 @@ public class Memory {
 			// Incrementamos el contador de las filas
 			i++;
 		}
-
-		// Si el número no ha sido encontrado
-		if (posX == -1 && posY == -1)
-			// El juego habrá terminado
-			juegoTerminado = true;
 
 		// Devolverá el valor de juegoTerminado
 		return juegoTerminado;
